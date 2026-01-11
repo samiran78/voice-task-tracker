be395894508df707c13ef11c2c497c779cf5fc24
Samiran Royy <sroy21191@gmail.com>
2026-01-12 01:20:16 +0530

    no message

---
+++ frontend/src/Api/tasks.js
@@ -0,0 +1,6 @@
+const API_BASE = 'http://localhost:5000/api';
+export async  function parseVoiceTask(transcript){
+    //here, we are identifying frontend by usuing fetch() with backend API.
+    
+    const response 
+}
